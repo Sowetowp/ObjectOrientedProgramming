c89562d02bb9e0e0465daac00b34b74bcca3ff23
Abdulmalik <ayodejiamzat@gmail.com>
2024-09-26 13:42:37 +0100

    auto-commit

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ class className {
 
 class profile {
     constructor(firstName, lastName, age, dob) {
-        this.firstName = firstName,
-            this.lastName = lastName,
-            this.age = age,
-            this.dob = dob,
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.dob = dob;
     }
 }
