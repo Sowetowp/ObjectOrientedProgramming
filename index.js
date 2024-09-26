a36536fdb240f1dc04e8a952a494ecdabf935b58
Abdulmalik <ayodejiamzat@gmail.com>
2024-09-26 15:42:15 +0100

    auto-commit

--- index.js
+++ index.js
@@ -85,3 +85,6 @@ const person = new Person('John', 'Doe');
 person.fullName = 'Jane Smith';  // Sets the new name
 console.log(person.fullName);
 // note how we didn't invoke fullname method
+
+
+// NOTE: Unlike functions, and other JavaScript declarations, class declarations are not hoisted. That means that you must declare a class before you can use it
