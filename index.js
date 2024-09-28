3f73c0f1cd43baaff8e864aabeb9e0727ef8a645
Abdulmalik <ayodejiamzat@gmail.com>
2024-09-28 23:33:58 +0100

    auto-commit

--- index.js
+++ index.js
@@ -154,23 +154,23 @@ class Animal {
 
 class Dog extends Animal {
     // Overriding the makeSound method
-    // makeSound() {
-    //     console.log("Dog barks: Woof Woof!");
-    // }
+    makeSound() {
+        console.log("Dog barks: Woof Woof!");
+    }
 }
 
 class Cat extends Animal {
     // Overriding the makeSound method
-    // makeSound() {
-    //     console.log("Cat meows: Meow Meow!");
-    // }
+    makeSound() {
+        console.log("Cat meows: Meow Meow!");
+    }
 }
 
 class Cow extends Animal {
     // Overriding the makeSound method
-    // makeSound() {
-    //     console.log("Cow moos: Moo Moo!");
-    // }
+    makeSound() {
+        console.log("Cow moos: Moo Moo!");
+    }
 }
 
 // Polymorphism in action
