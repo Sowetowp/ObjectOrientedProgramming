9db30a28552bc1fa6711982843288afa890f8417
Abdulmalik <ayodejiamzat@gmail.com>
2024-09-28 23:19:14 +0100

    auto-commit

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ console.log(Language.hello());
 
 // ABSTRACTION:used to hide unnecessary information and display only necessary information to the users interacting.
 // The internal method _startEngine() is a private method that handles the engine start, hidden from the user. When the user calls the start() method, they don't need to know the details of how the engine is started
-
+// The user interacts only with the public methods (start, accelerate, stop), while the complex details are abstracted away in the private method _startEngine()
 class Bus {
     constructor(brand) {
         this.brand = brand;
